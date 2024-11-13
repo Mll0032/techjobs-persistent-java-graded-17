3f4b6bfb2f6822dedc2490492021ce1435dfebfd
Mll0032 <mllawson18@gmail.com>
2024-11-13 16:59:30 -0600

    Updating Employer.java to add jobs List initialized in an ArrayList. Ensuring annotations of @OneToMany and @JoinColumn(name="employer_id). Job.java to extends abstract class and remove name, id with Employer field being of type employer and having @ManyToOne and @JoinColumn(name= "employer_id") Refactor getters and setters. HomeController.java to add field employerRepository with annotation @Autowired/ Add code to processAddJobForm to give access to employer from employerRepository. SQL Task write a statement to remove the job table.

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -1,49 +1,35 @@
 package org.launchcode.techjobs.persistent.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.List;
 
 @Entity
-public class Job {
+public class Job extends AbstractEntity{
 
-    @Id
-    @GeneratedValue
-    private int id;
+    @ManyToOne
+    @JoinColumn(name = "employer_id")
+    private Employer employer;
 
-    private String name;
-    private String employer;
     private String skills;
-
 
     public Job() {
     }
 
     // Initialize the id and value fields.
-    public Job(String anEmployer, String someSkills) {
+    public Job(Employer employer, String someSkills) {
         super();
-        this.employer = anEmployer;
+        this.employer = employer;
         this.skills = someSkills;
     }
 
     // Getters and setters.
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmployer() {
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
@@ -54,5 +40,4 @@ public class Job {
     public void setSkills(String skills) {
         this.skills = skills;
     }
-
 }
