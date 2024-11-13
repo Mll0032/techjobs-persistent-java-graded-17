6c67c830fa52dba0b9f98d50542424b14055172d
Mll0032 <mllawson18@gmail.com>
2024-11-12 22:07:53 -0600

    Update Employer.java to add the location field making sure to annotate with @NotNull so it cannot be empty and map it with @Entity, ad a no-arg constructor. Update Skill.java to have a description field, add a no-arg constructor. Add the interfaces Employer and Skill to the data layer. Both should have the @Repository annotation and extend the CrudRepository.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    private String location;
+
+    public Employer() {
+    }
 }
